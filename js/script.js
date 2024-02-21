a9b74cfe14e0516a8a9064202945de4128764909
Fortune Samson <mr4chun4u@gmail.com>
2024-02-21 10:47:17 +0100

    Added Search Functionality

--- js/script.js
+++ js/script.js
@@ -1,5 +1,16 @@
 const global = {
   currentPage: window.location.pathname,
+  search: {
+    term: '',
+    type: '',
+    page: 1,
+    totalPages: 1,
+    totalResults: 0,
+  },
+  api: {
+    apiKey: '8b3fb32765d319b6f5497a3f87b0fe73',
+    apiUrl: 'https://api.themoviedb.org/3/',
+  },
 };
 async function displayPopularMovies() {
   const { results } = await fetchAPIData('movie/popular');
@@ -210,6 +221,128 @@ async function displayTvDetails() {
         </div>`;
   document.querySelector('#show-details').appendChild(div);
 }
+// Search Movies/Shows
+async function search() {
+  const queryString = window.location.search;
+  const urlParams = new URLSearchParams(queryString);
+
+  global.search.type = urlParams.get('type');
+  global.search.term = urlParams.get('search-term');
+
+  //if you submit while empty it's gonna return null so test for that
+  if (global.search.term !== '' && global.search.term !== null) {
+    const { results, total_pages, page, total_results } = await searchAPIData();
+
+    global.search.page = page;
+    global.search.totalPages = total_pages;
+    global.search.totalResults = total_results;
+
+    if (results.length === 0) {
+      showAlert('No results found');
+      return;
+    }
+
+    displaySearchResults(results);
+
+    document.querySelector('#search-term').value = '';
+  } else {
+    showAlert('Please enter a search term');
+  }
+}
+
+function displaySearchResults(results) {
+  results.forEach((result) => {
+    const div = document.createElement('div');
+    div.classList.add('card');
+    div.innerHTML = `
+          <a href="${global.search.type}-details.html?id=${result.id}">
+           ${
+             result.poster_path
+               ? `
+            <img
+            src="https://image.tmdb.org/t/p/w500${result.poster_path}"
+            class="card-img-top"
+            alt="${global.search.type === 'movie' ? result.title : result.name}"
+          />`
+               : `
+          <img 
+          src="images/no-image.jpg"
+          class="card-img-top"
+          alt="${global.search.type === 'movie' ? result.title : result.name}"
+        />`
+           }
+          </a>
+          <div class="card-body">
+            <h5 class="card-title">${
+              global.search.type === 'movie' ? result.title : result.name
+            }</h5>
+            <p class="card-text">
+              <small class="text-muted">Release: ${
+                global.search.type === 'movie'
+                  ? result.release_date
+                  : result.first_air_date
+              }</small>
+            </p>
+          </div>
+        `;
+    document.querySelector('#search-results-heading').innerHTML = `
+              <h2>${results.length} of ${global.search.totalResults} Results for ${global.search.term}</h2>
+    `;
+    document.querySelector('#search-results').appendChild(div);
+  });
+}
+
+// Display Slider Movies
+async function displaySlider() {
+  const { results } = await fetchAPIData('movie/now_playing');
+
+  results.forEach((movie) => {
+    const div = document.createElement('div');
+    div.classList.add('swiper-slide');
+    div.innerHTML = `
+
+  <a href="movie-details.html?id=${movie.id}">
+   ${`<img
+    src="https://image.tmdb.org/t/p/w500${movie.poster_path}"
+    
+    alt="${movie.title}"
+  /> `}
+  </a>
+  <h4 class="swiper-rating">
+    <i class="fas fa-star text-secondary"></i> ${movie.vote_average.toFixed(
+      1
+    )} / 10
+  </h4>
+`;
+    document.querySelector('.swiper-wrapper').appendChild(div);
+
+    initSwiper();
+  });
+
+  function initSwiper() {
+    const swiper = new Swiper('.swiper', {
+      slidesPerView: 1,
+      spaceBetween: 30,
+      freeMode: true,
+      loop: true,
+      autoplay: {
+        delay: 3000,
+        disableOnInteraction: false,
+      },
+      breakpoints: {
+        500: {
+          slidesPerView: 2,
+        },
+        700: {
+          slidesPerView: 3,
+        },
+        1200: {
+          slidesPerView: 4,
+        },
+      },
+    });
+  }
+}
 
 // Display Backdrop on Details Page
 function displayBackgroundImage(type, backgroundPath) {
@@ -235,8 +368,8 @@ function displayBackgroundImage(type, backgroundPath) {
 
 // Fetch data from TMDB API
 async function fetchAPIData(endpoint) {
-  const API_KEY = '8b3fb32765d319b6f5497a3f87b0fe73';
-  const API_URL = 'https://api.themoviedb.org/3/';
+  const API_KEY = global.api.apiKey;
+  const API_URL = global.api.apiUrl;
 
   showSpinner();
 
@@ -250,6 +383,21 @@ async function fetchAPIData(endpoint) {
   return data;
 }
 
+async function searchAPIData() {
+  const API_KEY = global.api.apiKey;
+  const API_URL = global.api.apiUrl;
+
+  showSpinner();
+
+  const response = await fetch(
+    `${API_URL}search/${global.search.type}?api_key=${API_KEY}&language=en-US&query=${global.search.term}`
+  );
+  const data = await response.json();
+
+  hideSpinner();
+
+  return data;
+}
 function showSpinner() {
   document.querySelector('.spinner').classList.add('show');
 }
@@ -266,7 +414,18 @@ function highlightActiveLink() {
     }
   });
 }
+// Show Alert
+function showAlert(message, className = 'error') {
+  const alertEl = document.createElement('div');
+  alertEl.classList.add('alert', className);
+  alertEl.appendChild(document.createTextNode(message));
 
+  document.querySelector('#alert').appendChild(alertEl);
+
+  setTimeout(() => alertEl.remove(), 3000);
+}
+
+// Add commas to number
 function addCommasToNumber(number) {
   return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
 }
@@ -276,6 +435,7 @@ function init() {
     case '/':
     case '/index.html':
       //   console.log('Home');
+      displaySlider();
       displayPopularMovies();
       break;
     case '/shows.html':
@@ -288,10 +448,11 @@ function init() {
       break;
     case '/tv-details.html':
       //   console.log('Tv Details');
-      displayTvDetails()
+      displayTvDetails();
       break;
     case '/search.html':
-      console.log('Search');
+      // console.log('Search');
+      search();
       break;
   }
   highlightActiveLink();
